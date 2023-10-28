844db020f1687d740a040d1a9ad6fb4b7573efe9
atamayev <actamayev@gmail.com>
2023-10-28 14:47:12 -0400

    Add Linter, github workflow, more
    Install necessary backend dependencies
    Create dummy index.ts file

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv"
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+dotenv.config()
+
+const port = process.env.PORT || 8000
+
+const app = express()
+
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", req.headers.origin as string)
+	res.header("Access-Control-Allow-Credentials", "true")
+	next()
+})
+
+app.use(cors({
+	credentials: true,
+	origin: (origin, callback) => {
+		callback(null, true)
+	}
+}))
+
+app.use(cookieParser())
+app.use(express.json())
+
+//Add Routes here, like this:
+// app.use("/api/auth", authRoutes)
+app.use("*", (req, res) => res.status(404).json({ error: "Route not found"}))
+
+// Initialization of server:
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
