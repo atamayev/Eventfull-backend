5f7e022d0d70bfc3b49d839d708fdfeada1048d3
atamayev <actamayev@gmail.com>
2023-11-01 12:02:57 -0400

    Add login with google endpoint

--- src/controllers/auth/google-auth/generate-google-calendar-auth-url.ts
+++ src/controllers/auth/google-auth/generate-google-calendar-auth-url.ts
@@ -1,12 +1,13 @@
 import { Response, Request } from "express"
 import createGoogleAuthClient from "../../../utils/google/create-google-auth-client"
 
-export default function generateGoogleAuthUrl(req: Request, res: Response): Response {
+export default function generateGoogleCalendarAuthUrl(req: Request, res: Response): Response {
 	const oauth2Client = createGoogleAuthClient("http://localhost:8080/api/auth/google-auth/callback")
 
 	const authUrl = oauth2Client.generateAuthUrl({
 		access_type: "offline",
 		scope: [
+			"https://www.googleapis.com/auth/calendar",
 			"https://www.googleapis.com/auth/userinfo.email",
 			"https://www.googleapis.com/auth/userinfo.profile",
 			"openid"
--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,12 +1,13 @@
 import express from "express"
 import register from "../controllers/auth/register"
 import login from "../controllers/auth/login"
-import validateLoginRequestBody from "../middleware/request-validation/auth-routes/validate-login-request-body"
 import changePassword from "../controllers/auth/change-password"
 import googleAuthCallback from "../controllers/auth/google-auth/google-auth-callback"
 import generateGoogleAuthUrl from "../controllers/auth/google-auth/generate-google-auth-url"
+import generateGoogleCalendarAuthUrl from "../controllers/auth/google-auth/generate-google-calendar-auth-url"
 
 import jwtVerify from "../middleware/jwt-verify"
+import validateLoginRequestBody from "../middleware/request-validation/auth-routes/validate-login-request-body"
 import validateRegisterRequestBody from "../middleware/request-validation/auth-routes/validate-register-request-body"
 import validateGoogleAuthRequest from "../middleware/request-validation/auth-routes/validate-google-auth-request"
 import validateChangePasswordRequest from "../middleware/request-validation/auth-routes/validate-change-password-request-body"
@@ -18,6 +19,7 @@ authRoutes.post("/login", validateLoginRequestBody, login)
 authRoutes.post("/change-password", jwtVerify, validateChangePasswordRequest, changePassword)
 
 authRoutes.get("/google-auth/generate-auth-url", generateGoogleAuthUrl)
+authRoutes.get("/google-auth/generate-calendar-auth-url", generateGoogleCalendarAuthUrl)
 authRoutes.get("/google-auth/callback", validateGoogleAuthRequest, googleAuthCallback)
 
 export default authRoutes
