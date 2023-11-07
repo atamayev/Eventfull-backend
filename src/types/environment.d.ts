f3a19f81d3fefa94e843e272f69113d2d2da07ee
atamayev <75034340+atamayev@users.noreply.github.com>
2023-11-06 22:10:31 -0500

    Renaming and adding packages

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -14,6 +14,10 @@ declare global {
 			GOOGLE_CLIENT_ID: string
 			GOOGLE_CLIENT_SECRET: string
 
+			// Microsoft Auth:
+			MICROSOFT_CLIENT_ID: string
+			MICROSOFT_SECRET_ID: string
+
 			PORT: string
 		}
 	}
