2c5a5082883f736eb71025a9e041de49430b0f24
atamayev <75034340+atamayev@users.noreply.github.com>
2023-11-24 00:17:43 -0500

    Create microsoft graph client

--- src/controllers/calendar/microsoft/get-microsoft-calendar-details.ts
+++ src/controllers/calendar/microsoft/get-microsoft-calendar-details.ts
@@ -1,30 +1,24 @@
 import _ from "lodash"
 import { Response, Request } from "express"
-import axios, { AxiosResponse } from "axios"
 import { Event } from "@microsoft/microsoft-graph-types"
 import convertMicrosoftToUnified from "../../../utils/microsoft/calendar-retrieval/convert-microsoft-to-unified"
 import saveOrUpdateUserCalendarEvents from "../../../utils/save-or-update-incoming-calendar-data"
+import createGraphClient from "../../../utils/microsoft/create-graph-client"
 
 export default async function getMicrosoftCalendarDetails(req: Request, res: Response): Promise<Response> {
 	try {
 		const userId = req.userId
 		const microsoftCalendarAccessToken = req.headers.microsoftCalendarAccessToken
-		if (_.isUndefined(microsoftCalendarAccessToken)) {
-			return res.status(400).json({ error: "No Microsoft Calendar Access Token Found" })
+		if (!_.isString(microsoftCalendarAccessToken)) {
+			return res.status(400).json({ error: "Invalid Microsoft Calendar Access Token" })
 		}
 
 		const calendarId = req.headers.microsoftDefaultCalendarId as string
+		const client = createGraphClient(microsoftCalendarAccessToken)
 
-		const url = `https://graph.microsoft.com/v1.0/me/calendars/${calendarId}/events`
-		const response: AxiosResponse<{ value: Event[] }> = await axios.get(url, {
-			headers: {
-				Authorization: `Bearer ${microsoftCalendarAccessToken}`,
-				"Content-Type": "application/json"
-			}
-		})
+		const calendarDetails = await client.api(`/me/calendars/${calendarId}/events`).get() as { value: Event[] }
 
-		const calendarDetails = response.data.value
-		const unifiedMicrosoftCalendarDetails = convertMicrosoftToUnified(calendarDetails)
+		const unifiedMicrosoftCalendarDetails = convertMicrosoftToUnified(calendarDetails.value)
 		await saveOrUpdateUserCalendarEvents(userId, unifiedMicrosoftCalendarDetails)
 
 		return res.status(200).json({ calendarDetails: unifiedMicrosoftCalendarDetails })
