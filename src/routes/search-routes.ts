d3b87afcaf292a2c312e4696862781bb4907614d
atamayev <75034340+atamayev@users.noreply.github.com>
2023-11-25 20:42:32 -0500

    Update search-routes.ts

--- src/routes/search-routes.ts
+++ src/routes/search-routes.ts
@@ -1,11 +1,10 @@
 import express from "express"
 import searchForUsername from "../controllers/search/search-for-username"
 
-import jwtVerify from "../middleware/jwt-verify"
 import validateUsernameInParams from "../middleware/request-validation/search-routes/validate-username-in-params"
 
 const searchRoutes = express.Router()
 
-searchRoutes.get("/username/:username", jwtVerify, validateUsernameInParams, searchForUsername)
+searchRoutes.get("/username/:username", validateUsernameInParams, searchForUsername)
 
 export default searchRoutes
