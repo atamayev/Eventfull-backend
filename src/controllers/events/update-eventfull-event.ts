9a18a5fdbbe5671c45bd27e10655ad73bcaf8219
atamayev <75034340+atamayev@users.noreply.github.com>
2023-12-17 12:20:56 -0500

    Update update-eventfull-event.ts

--- src/controllers/events/update-eventfull-event.ts
+++ src/controllers/events/update-eventfull-event.ts
@@ -10,7 +10,6 @@ export default async function updateEventfullEvent(req: Request, res: Response):
 		const eventfullEventId = req.body.eventfullEventId as string
 		const updatedEventData  = req.body.eventfullEventData as IncomingEventfullEvent
 		const organizerOrCoHost = req.organizerOrCoHost as "Organizer" | "Co-Host"
-		// If Organizer, can add/delete co-hosts. If Co-Host, cannot add co-hosts
 
 		const currentEvent = await EventfullEventModel.findById(eventfullEventId)
 		if (_.isNull(currentEvent)) return res.status(404).json({ error: "Event not found" })
@@ -18,6 +17,8 @@ export default async function updateEventfullEvent(req: Request, res: Response):
 		const user = await UserModel.findById(userId).select("friends")
 		const friendIds = user?.friends.map(friend => friend.toString()) || []
 
+		// Removes invitees that are not friends of the organizer,
+		// Maps each invitee to be invited by the user, and set the status as Not Responded
 		const newInvitees = updatedEventData.invitees
 			.filter(inviteeId => friendIds.includes(inviteeId.toString()))
 			.map(inviteeId => ({
@@ -26,12 +27,14 @@ export default async function updateEventfullEvent(req: Request, res: Response):
 				invitedBy: userId,
 			}))
 
+		// Removes invitees that are already invited to the event from being added again
 		const inviteesToAdd = newInvitees.filter(newInvitee =>
 			!currentEvent.invitees.some(existingInvitee =>
 				existingInvitee.userId.toString() === newInvitee.userId.toString()
 			)
 		)
 
+		// Removes invitees in the current event whose IDs are not in the updated event's invitees, but only if they have not responded yet
 		const inviteesToRemove = currentEvent.invitees.filter(existingInvitee =>
 			!updatedEventData.invitees.some(newInviteeId =>
 				existingInvitee.userId === newInviteeId
@@ -82,6 +85,53 @@ export default async function updateEventfullEvent(req: Request, res: Response):
 			...addInviteesPromises,
 			...removeInviteesPromises
 		])
+
+		if (organizerOrCoHost === "Organizer") {
+			const currentHostIds = currentEvent.coHosts.map(host => host.toString())
+			const newHostIds = updatedEventData.coHosts.map(host => host.toString())
+			newHostIds.push(userId.toString())
+			const coHostsToAdd = newHostIds.filter(coHostId => !currentHostIds.includes(coHostId))
+			const coHostsToRemove = currentHostIds.filter(existingCoHost => !newHostIds.includes(existingCoHost))
+
+			if (!_.isEmpty(coHostsToRemove)) {
+				await EventfullEventModel.findByIdAndUpdate(
+					eventfullEventId,
+					{ $pull: { coHosts: { $in: coHostsToRemove } } },
+					{ new: true, runValidators: true }
+				)
+				const removeCoHostsPromises = coHostsToRemove.map(coHostId =>
+					UserModel.updateOne(
+						{ _id: coHostId },
+						{ $pull: { eventfullEvents: { eventId: eventfullEventId } } }
+					)
+				)
+				await Promise.all(removeCoHostsPromises)
+			}
+
+			if (!_.isEmpty(coHostsToAdd)) {
+				await EventfullEventModel.findByIdAndUpdate(
+					eventfullEventId,
+					{ $addToSet: { coHosts: { $each: coHostsToAdd } } },
+					{ new: true, runValidators: true }
+				)
+
+				const addCoHostsPromises = coHostsToAdd.map(coHostId =>
+					UserModel.updateOne(
+						{ _id: coHostId },
+						{
+							$push: {
+								eventfullEvents: {
+									eventId: eventfullEventId,
+									attendingStatus: "Co-Hosting",
+									invitedBy: userId
+								}
+							}
+						})
+				)
+
+				await Promise.all(addCoHostsPromises)
+			}
+		}
 
 		return res.status(200).json({ message: "Event Updated" })
 	} catch (error) {
