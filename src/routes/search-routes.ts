4baa26fbe69c7b56fa3518bf8ce479412858ba51
atamayev <75034340+atamayev@users.noreply.github.com>
2023-12-18 21:47:01 -0500

    Search by event name

--- src/routes/search-routes.ts
+++ src/routes/search-routes.ts
@@ -2,9 +2,12 @@ import express from "express"
 import searchForUsername from "../controllers/search/search-for-username"
 
 import validateUsernameInParams from "../middleware/request-validation/search/validate-username-in-params"
+import validateEventNameInParams from "../middleware/request-validation/search/validate-event-name-in-params"
+import searchForEventName from "../controllers/search/search-for-event"
 
 const searchRoutes = express.Router()
 
 searchRoutes.get("/username/:username", validateUsernameInParams, searchForUsername)
+searchRoutes.get("/event-name/:eventName", validateEventNameInParams, searchForEventName)
 
 export default searchRoutes
