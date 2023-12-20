7693826434325ea74894d6ada46a976c18d8363a
atamayev <75034340+atamayev@users.noreply.github.com>
2023-12-19 19:51:49 -0500

    Ability to verify phone, even if non primary contact

--- src/controllers/auth/twilio/verify-user-phone-code.ts
+++ src/controllers/auth/twilio/verify-user-phone-code.ts
@@ -22,7 +22,7 @@ export default async function verifyUserPhoneCode(req: Request, res: Response): 
 			phoneVerificationCodeTimestamp: null,
 		})
 
-		return res.status(200).json({ message: "Code is Valid. Phone Number Verified" })
+		return res.status(200).json({ message: "Phone Number Verified" })
 	} catch (error) {
 		console.error(error)
 		return res.status(500).json({ error: "Internal Server Error: Unable to Send Verification Code" })
--- src/utils/auth-helpers/add-secondary-contact-method-to-db.ts
+++ src/utils/auth-helpers/add-secondary-contact-method-to-db.ts
@@ -3,6 +3,8 @@ import UserModel from "../../models/user-model"
 interface UserFields {
 	email?: string
 	phoneNumber?: string
+	isPhoneVerified?: boolean
+	isEmailVerified?: boolean
 }
 
 export default async function addSecondaryContactMethodToDb (user: User, contact: string, contactType: EmailOrPhone): Promise<null | void> {
@@ -12,8 +14,13 @@ export default async function addSecondaryContactMethodToDb (user: User, contact
 		}
 
 		const updateFields: UserFields = {}
-		if (contactType === "Email") updateFields.email = contact
-		else updateFields.phoneNumber = contact
+		if (contactType === "Email") {
+			updateFields.email = contact
+			updateFields.isEmailVerified = false
+		} else {
+			updateFields.phoneNumber = contact
+			updateFields.isPhoneVerified = false
+		}
 
 		await UserModel.findByIdAndUpdate(user._id, updateFields, { runValidators: true })
 	} catch (error) {
