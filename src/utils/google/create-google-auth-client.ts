2b5604d6a2c57a1145e100e1581f6b5c6ca63c8d
atamayev <75034340+atamayev@users.noreply.github.com>
2023-12-23 10:55:37 -0500

    Update create-google-auth-client.ts

--- src/utils/google/create-google-auth-client.ts
+++ src/utils/google/create-google-auth-client.ts
@@ -1,9 +1,8 @@
 import { OAuth2Client } from "google-auth-library"
 
-export default function createGoogleAuthClient(callbackUrl?: string): OAuth2Client {
+export default function createGoogleAuthClient(): OAuth2Client {
 	return new OAuth2Client(
 		process.env.GOOGLE_CLIENT_ID,
-		process.env.GOOGLE_CLIENT_SECRET,
-		callbackUrl
+		process.env.GOOGLE_CLIENT_SECRET
 	)
 }
