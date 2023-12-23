29b2024f5e7c44c80e87135bd6846ff789fb62aa
atamayev <75034340+atamayev@users.noreply.github.com>
2023-12-23 11:26:36 -0500

    Remove unnecessary auth method checks

--- src/utils/auth-helpers/retrieve-user-from-contact.ts
+++ src/utils/auth-helpers/retrieve-user-from-contact.ts
@@ -5,17 +5,14 @@ export default async function retrieveUserFromContact(contact: string, contactTy
 	if (contactType === "Email") {
 		user = await UserModel.findOne({
 			email: { $regex: `^${contact}$`, $options: "i" },
-			authMethod: { $in: ["local", "google", "microsoft"] }
 		})
 	} else if (contactType === "Phone") {
 		user = await UserModel.findOne({
 			phoneNumber: { $regex: `^${contact}$`, $options: "i" },
-			authMethod: { $in: ["local", "google", "microsoft"] }
 		})
 	} else {
 		user = await UserModel.findOne({
 			username: { $regex: `^${contact}$`, $options: "i" },
-			authMethod: { $in: ["local", "google", "microsoft"] }
 		})
 	}
 
