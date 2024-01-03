bf228ee28ae4328e30dc5fd22a183ecde74b1da0
atamayev <75034340+atamayev@users.noreply.github.com>
2024-01-03 12:04:27 -0500

    Delete create-jwt-payload.ts

--- src/utils/auth-helpers/jwt/create-jwt-payload.ts
+++
@@ -1,9 +0,0 @@
-import _ from "lodash"
-import { Types } from "mongoose"
-
-export default function createJWTPayload (userId: Types.ObjectId, isNewUser: boolean = false): JwtPayload {
-	return {
-		userId: _.toString(userId),
-		newUser: isNewUser
-	}
-}
