8ef627c0ea4bd20dc5d0112e1e3a46189d7de5ab
atamayev <75034340+atamayev@users.noreply.github.com>
2024-01-05 16:26:12 -0500

    Update retrieve-direct-message-chats.ts

--- src/controllers/social/chat/direct/retrieve-direct-message-chats.ts
+++ src/controllers/social/chat/direct/retrieve-direct-message-chats.ts
@@ -7,8 +7,6 @@ export default async function retrieveDirectMessageChats(req: Request, res: Resp
 		const user = req.user
 		const userDirectMessageChats = user.directMessageChats
 
-		// TODO: Also return the user's chat name. will have to first go through the user schema and add a chat name field.
-		// do the same for group chats
 		if (_.isEmpty(userDirectMessageChats)) {
 			return res.status(200).json({ directMessageChats: [] })
 		}
