5d56ecbcfa44494d8908561e3383cd04f0412b4a
atamayev <75034340+atamayev@users.noreply.github.com>
2024-01-20 09:22:42 -0500

    Message retrieval bug fix

--- src/controllers/chat/private/message/retrieve-private-chat-messages.ts
+++ src/controllers/chat/private/message/retrieve-private-chat-messages.ts
@@ -4,9 +4,8 @@ import PrivateMessageModel from "../../../../models/chat/private/private-message
 
 export default async function retrievePrivateChatMessages(req: Request, res: Response): Promise<Response> {
 	try {
-		// TODO: Need middleware to validate these new fields
 		const privateChat = req.privateChat
-		const { lastMessageId, limit = 20 } = req.query as { lastMessageId?: string; limit?: number }
+		const { lastMessageId, limit } = req.query as { lastMessageId?: string; limit?: string }
 
 		interface QueryType {
 			privateChatId: typeof privateChat._id;
@@ -18,13 +17,15 @@ export default async function retrievePrivateChatMessages(req: Request, res: Res
 			query = { ...query, _id: { $lt: lastMessageId } }
 		}
 
+		const limitNumber = parseInt(limit || "20", 10)
+
 		const privateMessages = await PrivateMessageModel.find(query)
 			.sort({ createdAt: -1 })
-			.limit(limit + 1) // Fetch one extra message for checking
+			.limit(limitNumber + 1) // Fetch one extra message for checking
 			.lean()
 			.exec()
 
-		const hasMore = privateMessages.length > limit
+		const hasMore = privateMessages.length > limitNumber
 		const messagesToSend = hasMore ? privateMessages.slice(0, -1) : privateMessages
 
 		return res.status(200).json({ privateMessages: messagesToSend, hasMore })
