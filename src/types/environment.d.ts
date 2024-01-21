54ed63c43d47d4502b8c6120d050dcbd8391ee40
atamayev <75034340+atamayev@users.noreply.github.com>
2024-01-20 22:25:01 -0500

    Cleanup

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -11,9 +11,6 @@ declare namespace NodeJS {
 		// MongoDB:
 		MONGODB_URI: string
 
-		// Frontend:
-		FRONTEND_URL: string
-
 		// Google Auth:
 		GOOGLE_CLIENT_ID: string
 		GOOGLE_CLIENT_SECRET: string
@@ -22,11 +19,6 @@ declare namespace NodeJS {
 		MICROSOFT_CLIENT_ID: string
 		MICROSOFT_SECRET_ID: string
 		MICROSOFT_TENANT_ID: string
-
-		// Twilio Auth
-		TWILIO_ACCOUNT_SID: string
-		TWILIO_AUTH_TOKEN: string
-		TWILIO_PHONE_NUMBER: string
 
 		// SendGrid Auth
 		SENDGRID_API_KEY: string
