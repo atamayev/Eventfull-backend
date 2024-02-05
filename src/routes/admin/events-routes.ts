da552b0cadd9ee08026a0ff4a94e30049325303b
atamayev <75034340+atamayev@users.noreply.github.com>
2024-02-04 23:14:43 -0500

    Update events-routes.ts

--- src/routes/admin/events-routes.ts
+++ src/routes/admin/events-routes.ts
@@ -1,18 +1,18 @@
 import express from "express"
 
 import validateEventId from "../../middleware/request-validation/events/validate-event-id"
+import validateImageURLs from "../../middleware/request-validation/admin/events/validate-image-urls"
 import confirmEventFrequencyAttributes from "../../middleware/events/confirm-event-frequency-attributes"
+import validateEventfullEventId from "../../middleware/request-validation/events/validate-eventfull-event-id"
 import validateCreateEventfullEvent from "../../middleware/request-validation/events/validate-create-eventfull-event"
 import validateUpdateEventfullEvent from "../../middleware/request-validation/events/validate-update-eventfull-event"
 
+import addImageURLs from "../../controllers/admin/events/add-image-urls"
 import addAdminEventfullEvent from "../../controllers/admin/events/add-admin-eventfull-event"
 import retrieveEventfullEvents from "../../controllers/admin/events/retrieve-eventfull-events"
 import updateAdminEventfullEvent from "../../controllers/admin/events/update-admin-eventfull-event"
-import retrieveSingleEventfullEvent from "../../controllers/admin/events/retrieve-single-eventfull-event"
 import deleteAdminEventfullEvent from "../../controllers/admin/events/delete-admin-eventfull-event"
-import addImageURLs from "../../controllers/admin/events/add-image-urls"
-import validateEventfullEventId from "../../middleware/request-validation/events/validate-eventfull-event-id"
-import validateImageURLs from "../../middleware/request-validation/admin/events/validate-image-urls"
+import retrieveSingleEventfullEvent from "../../controllers/admin/events/retrieve-single-eventfull-event"
 
 const adminEventsRoutes = express.Router()
 
