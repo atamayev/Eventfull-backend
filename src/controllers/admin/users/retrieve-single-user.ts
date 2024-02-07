ca67f385df31823a15c8fe2007d9de3a178c5f4f
atamayev <75034340+atamayev@users.noreply.github.com>
2024-02-06 22:07:29 -0500

    View user endpoint

---
+++ src/controllers/admin/users/retrieve-single-user.ts
@@ -0,0 +1,18 @@
+import _ from "lodash"
+import { Types } from "mongoose"
+import { Request, Response } from "express"
+import findUser from "../../../utils/find/find-user"
+
+export default async function retrieveSingleUser(req: Request, res: Response): Promise<Response> {
+	try {
+		const userId = req.params.userId as string
+		const user = await findUser(userId as unknown as Types.ObjectId)
+
+		if (_.isNull(user))	return res.status(400).json({ message: "User not found" })
+
+		return res.status(200).json({ user })
+	} catch (error) {
+		console.error(error)
+		return res.status(500).json({ error: "Internal Server Error: Unable to Create User" })
+	}
+}
--- src/routes/users/users-routes.ts
+++ src/routes/users/users-routes.ts
@@ -1,9 +1,14 @@
 import express from "express"
 
 import retrieveUsers from "../../controllers/admin/users/retrieve-users"
+import retrieveSingleUser from "../../controllers/admin/users/retrieve-single-user"
+
+import validateUserId from "../../middleware/request-validation/admin/users/validate-user-id"
 
 const adminUsersRoutes = express.Router()
 
 adminUsersRoutes.get("/get-users", retrieveUsers)
+
+adminUsersRoutes.get("/get-user/:userId", validateUserId, retrieveSingleUser)
 
 export default adminUsersRoutes
