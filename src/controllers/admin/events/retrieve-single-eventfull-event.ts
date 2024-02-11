e626bedb598d99ef766122fb6f3e228741a0cfc0
atamayev <75034340+atamayev@users.noreply.github.com>
2024-02-11 12:39:39 -0500

    Code Simplification

--- src/controllers/admin/events/retrieve-single-eventfull-event.ts
+++ src/controllers/admin/events/retrieve-single-eventfull-event.ts
@@ -1,19 +1,15 @@
 import _ from "lodash"
-import { Types } from "mongoose"
 import { Request, Response } from "express"
-import findEvent from "../../../utils/find/find-event"
+import EventfullEventModel from "../../../models/eventfull-event-model"
 
 export default async function retrieveSingleEventfullEvent(req: Request, res: Response): Promise<Response> {
 	try {
 		const eventId = req.params.eventId as string
-		const foundEvent = await findEvent(eventId as unknown as Types.ObjectId)
-		if (_.isNull(foundEvent)) return res.status(400).json({ message: "Event not found" })
+		const event = await EventfullEventModel.findById(eventId).lean()
+		if (_.isNull(event)) return res.status(400).json({ message: "Event not found" })
 
-		const event = foundEvent.toObject() as EventfullEvent
+		if (event.isActive !== true) return res.status(400).json({ message: "Event not found" })
 
-		if (event.isActive !== true) {
-			return res.status(400).json({ message: "Event not found" })
-		}
 		event.eventImages = event.eventImages.filter(image => image.isActive === true)
 
 		return res.status(200).json({ event })
