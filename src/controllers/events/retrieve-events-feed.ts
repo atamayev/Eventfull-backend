d1d2bdecfbed77677369d7d63cc8a95c763be9fa
atamayev <75034340+atamayev@users.noreply.github.com>
2024-02-12 14:45:15 -0500

    Only retrieve events that have images

--- src/controllers/events/retrieve-events-feed.ts
+++ src/controllers/events/retrieve-events-feed.ts
@@ -3,7 +3,16 @@ import EventfullEventModel from "../../models/eventfull-event-model"
 
 export default async function retrieveEventsFeed(req: Request, res: Response): Promise<Response> {
 	try {
-		const events = await EventfullEventModel.find({ isActive: true, eventPublic: true }).sort({ createdAt: "desc" })
+		const events = await EventfullEventModel.find({
+			isActive: true,
+			eventPublic: true,
+			eventImages: {
+				$elemMatch: { // Ensures at least one element in the array matches the following conditions
+					isActive: true,
+					imageURL: { $exists: true, $ne: "" } // Checks that imageURL exists and is not an empty string
+				}
+			}
+		}).sort({ createdAt: "desc" })
 
 		return res.status(200).json({ events })
 	} catch (error) {
