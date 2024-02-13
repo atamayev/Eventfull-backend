84b222e891de0b7fa03a07fd0186f6e95382fcda
atamayev <75034340+atamayev@users.noreply.github.com>
2024-02-12 21:10:09 -0500

    Retrieve events for feed

--- src/controllers/events/retrieve-events-feed.ts
+++ src/controllers/events/retrieve-events-feed.ts
@@ -1,18 +1,61 @@
 import { Request, Response } from "express"
 import EventfullEventModel from "../../models/eventfull-event-model"
 
+// eslint-disable-next-line max-lines-per-function
 export default async function retrieveEventsFeed(req: Request, res: Response): Promise<Response> {
 	try {
-		const events = await EventfullEventModel.find({
-			isActive: true,
-			eventPublic: true,
-			eventImages: {
-				$elemMatch: { // Ensures at least one element in the array matches the following conditions
+		const currentDate = new Date()
+		const events = await EventfullEventModel.aggregate([
+		// Match initial conditions
+			{
+				$match: {
 					isActive: true,
-					imageURL: { $exists: true, $ne: "" } // Checks that imageURL exists and is not an empty string
+					eventPublic: true,
+					eventImages: {
+						$elemMatch: {
+							isActive: true,
+							imageURL: { $exists: true, $ne: "" }
+						}
+					}
 				}
-			}
-		}).sort({ createdAt: "desc" })
+			},
+
+			// Determine if the event is in the future
+			{
+				$addFields: {
+					isInFuture: {
+						$switch: {
+							branches: [
+								{
+									case: { $eq: ["$eventFrequency", "one-time"] },
+									then: { $gt: ["$singularEventTime.startTime", currentDate] }
+								},
+								{
+									case: { $eq: ["$eventFrequency", "custom"] },
+									then: {
+										$gt: [
+											{ $max: "$customEventDates.startTime" },
+											currentDate
+										]
+									}
+								},
+								{
+									case: { $eq: ["$eventFrequency", "ongoing"] },
+									then: true // Assuming ongoing events are always considered in the future; adjust as needed
+								}
+							],
+							default: false
+						}
+					}
+				}
+			},
+
+			// Filter documents that are in the future
+			{ $match: { isInFuture: true } },
+
+			// Sort by createdAt in descending order
+			{ $sort: { createdAt: -1 } }
+		])
 
 		return res.status(200).json({ events })
 	} catch (error) {
