824158a8ecf3b976e5cf418457831c56b88f1cc2
atamayev <75034340+atamayev@users.noreply.github.com>
2024-02-17 18:09:42 -0500

    Return the user id with the login/register response

--- src/controllers/auth/register.ts
+++ src/controllers/auth/register.ts
@@ -32,7 +32,7 @@ export default async function register (req: Request, res: Response): Promise<Re
 
 		await addLoginRecord(userId)
 
-		return res.status(200).json({ accessToken })
+		return res.status(200).json({ userId, accessToken })
 	} catch (error) {
 		console.error(error)
 		return res.status(500).json({ error: "Internal Server Error: Unable to Register New User" })
